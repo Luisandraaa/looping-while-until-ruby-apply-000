7b1a37d5852c03a0211382dc50edd1a9349eb21e
luisandra abreu duran <abreuluisandra@gmail.com>
2020-06-09 02:27:34 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,6 +1,9 @@
+
 def using_while
   levitation_force = 0
   while levitation_force < 10
-  puts "Wingardium Leviosa"
-  levitation_force += 1
+    puts "Wingardium Leviosa"
+    levitation_force += 1
 end
+
+puts "Wingardium Leviosa"
